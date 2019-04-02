6bd1733ee38ae6154872b4d8c8538ed85bdd4b18
Ido Sheffer <Ido.Sheffer@tradency.com>
2019-04-02 17:39:53 +0300

    Added docker support

--- KubeMQpub/Program.cs
+++ KubeMQpub/Program.cs
@@ -6,71 +6,83 @@ namespace KubeMQpub
 {
     class Program
     {
-        //The KubeMQ communication channel between pub sub
-        const string eventChannelName = "TestCh_1";
-        private static bool pubLoop=false;
+        //The pub ClientID, can be set by environment var CLIENT
+        private static string ClientID = Environment.GetEnvironmentVariable("CLIENT")?? $"pub_Demo_{Environment.MachineName}";
+        //The KubeMQ communication channel between pub sub, can be set by environment var CHANNEL 
+        private static string  ChannelName = Environment.GetEnvironmentVariable("CHANNEL")??"Test_Channel";
 
+        private static bool pubLoop = true;
         static void Main(string[] args)
         {
-       
-          
-            //KubeMQ address can be set as environment parameter "KubeMQServerAddress"
-            //The address port is the Grpc communication KubeMQ port 
+            Console.WriteLine("[Demo] Start demo PUB");
+            //KubeMQ address environment var "KubeMQServerAddress" GRPC port
             Console.WriteLine($"[Demo] KUBEMQ GRPC address:{Environment.GetEnvironmentVariable("KubeMQServerAddress")}");
-            //Starting sync publisher
-            Console.WriteLine("[Demo] Starting demo PUB");
-            ChannelParameters eventChannelParameters = new ChannelParameters
+            Console.WriteLine($"[Demo] ClientID:{ClientID}");
+            Console.WriteLine($"[Demo] ChannelName:{ChannelName}");
+
+            Console.WriteLine("[Demo] press Ctrl+c to stop");
+
+            System.Threading.AutoResetEvent waitHandle = new System.Threading.AutoResetEvent(false);
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
             {
-                //Publish channel name (must match the sub ChannelName)
-                ChannelName = eventChannelName,
-                //pub name ID
-                ClientID = "pub_Demo"
+                pubLoop = false;
+                e.Cancel = true;              
+                waitHandle.Set();
+
             };
 
 
+            ChannelParameters eventChannelParameters = new ChannelParameters
+            {
+                //Publish channel name (must match the sub ChannelName)
+                ChannelName = ChannelName,
+                //pub name ID
+                ClientID = ClientID
+            };
 
             //Create a new KubeMQ.SDK.csharp.Events.Channel 
             var eChannel = new Channel(eventChannelParameters);
             //KubeMQ.SDK.csharp.Events.Result is the pub message status.
             Result result = null;
 
-            Console.WriteLine("[Demo] press Ctrl+c to stop");
-            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
-            {
-                pubLoop = false;
-                e.Cancel = true;
-            };
-
-
             Console.WriteLine("[Demo] Please enter message text to publish");
+
             while (pubLoop)
-            {
-            
-                string msg = Console.ReadLine();
-          
+            {            
+               string msg = Console.ReadLine();
+
+                if(msg==null)
+                {
+                    continue;
+                }
                 try
                 {
                     result = eChannel.SendEvent(new KubeMQ.SDK.csharp.Events.Event()
                     {
                         //KubeMQ.SDK.csharp.ToolsConverter.ToByteArray, be sure do decode with the same encoder on sub.
-                        Body = Converter.ToByteArray(msg)
+                        Body = Converter.ToByteArray(msg)                    
+                        
                     });
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"[Demo] Could not send message, please check KubeMQ address, Error{ex.GetType()}");
-                    continue;
+                    Console.WriteLine($"[Demo] Could not send messages, please check KubeMQ address, Error{ex.GetType()}");
+                    waitHandle.Set();
+                    break;
                 }
 
                 if (!result.Sent)
                 {
-                    Console.WriteLine($"[Demo] Could not send message:{result.Error}");
+                    Console.WriteLine($"[Demo] Could not send single message:{result.Error}");
                     continue;
                 }
 
                 Console.WriteLine($"[Demo] Sent message ID:{result.EventID}");
             }
+         
+            waitHandle.WaitOne();
 
-        }       
+
+        }
     }
 }
--- KubeMQsub/Program.cs
+++ KubeMQsub/Program.cs
@@ -6,29 +6,37 @@ using System;
 namespace KubeMQsub
 {
     class Program
-    {  //The KubeMQ communication channel between pub sub
-        const string eventChannelName = "TestCh_1";
-        private static bool subLoop=true;
+    {  
+    
+        //The sub ClientID, can be set by environment var CLIENT
+        private static string ClientID = Environment.GetEnvironmentVariable("CLIENT") ?? $"sub_Demo_{Environment.MachineName}";
+        //The KubeMQ communication channel between pub sub, can be set by environment var CHANNEL 
+        private static string ChannelName = Environment.GetEnvironmentVariable("CHANNEL") ?? "Test_Channel";
 
         static void Main(string[] args)
         {
-
-
-            //KubeMQ address can be set as environment parameter "KubeMQServerAddress"
-            //The address port is the Grpc communication KubeMQ port 
-            Console.WriteLine($"[Demo] KUBEMQ GRPC address:{Environment.GetEnvironmentVariable("KubeMQServerAddress")}");
-
-
-            //Start event subscriber
-
             Console.WriteLine("[Demo] Starting demo SUB");
+            //KubeMQ address environment var "KubeMQServerAddress" GRPC port         
+            Console.WriteLine($"[Demo] KUBEMQ GRPC address:{Environment.GetEnvironmentVariable("KubeMQServerAddress")}");
+            Console.WriteLine($"[Demo] ClientID:{ClientID}");
+            Console.WriteLine($"[Demo] ChannelName:{ChannelName}");
+
+            Console.WriteLine("[Demo] press Ctrl+c to stop");
+            System.Threading.AutoResetEvent waitHandle = new System.Threading.AutoResetEvent(false);
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+            {
+                e.Cancel = true;
+                waitHandle.Set();
+            };
+
+
 
             //create KubeMQ.SDK.csharp.Subscription.SubscribeRequest
             SubscribeRequest subscribeRequest = new SubscribeRequest()
             {
-                Channel = eventChannelName, //Subscribed channel name
+                Channel = ChannelName, //Subscribed channel name
                 SubscribeType = SubscribeType.Events, //sub channel pattern
-                ClientID = "sub_Demo"  //sub name ID
+                ClientID = ClientID  //sub name ID
             };
 
             //create a new KubeMQ.SDK.csharp.Events.Subscriber (KubeMQServerAddress environment var)
@@ -46,19 +54,9 @@ namespace KubeMQsub
             catch (Exception ex)
             {
                 Console.WriteLine($"[Demo] Did not subscribe, please check KubeMQ address, Error:{ex.GetType()}");
-            }
+            }           
 
-            Console.WriteLine("[Demo] press Ctrl+c to stop");
-            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
-            {
-                subLoop = false;
-                e.Cancel = true;
-            };
-            while (subLoop)
-            {
-                Console.ReadKey();
-            }
-
+            waitHandle.WaitOne();        
         }
     }
 }
