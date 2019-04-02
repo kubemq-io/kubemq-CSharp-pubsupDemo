7eed7d33400d936b8341402c327e71c6bc2ba9c5
Ido Sheffer <Ido.Sheffer@tradency.com>
2019-04-02 13:53:45 +0300

    split demo to pub and sub console apps

---
+++ KubeMQpub/Program.cs
@@ -0,0 +1,75 @@
+﻿using KubeMQ.SDK.csharp.Events;
+using KubeMQ.SDK.csharp.Tools;
+using System;
+
+namespace KubeMQpub
+{
+    class Program
+    {
+        const string eventChannelName = "TestCh_1";
+        private static bool pubLoop=false;
+
+        static void Main(string[] args)
+        {
+       
+          
+            //KubeMQ address can be set as environment parameter "KubeMQServerAddress"
+            //The address port is the Grpc communication KubeMQ port 
+            Console.WriteLine($"[Demo] KUBEMQ GRPC address:{Environment.GetEnvironmentVariable("KubeMQServerAddress")}");
+            //Starting sync publisher
+            Console.WriteLine("[Demo] Starting demo PUB");
+            ChannelParameters eventChannelParameters = new ChannelParameters
+            {
+                //Publish channel name (must match the sub ChannelName)
+                ChannelName = eventChannelName,
+                //pub name ID
+                ClientID = "pub_Demo"
+            };
+
+
+
+            //Create a new KubeMQ.SDK.csharp.Events.Channel 
+            var eChannel = new Channel(eventChannelParameters);
+            //KubeMQ.SDK.csharp.Events.Result is the pub message status.
+            Result result = null;
+
+            Console.WriteLine("[Demo] press Ctrl+c to stop");
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+            {
+                pubLoop = false;
+                e.Cancel = true;
+            };
+
+
+            Console.WriteLine("[Demo] Please enter message text to publish");
+            while (pubLoop)
+            {
+            
+                string msg = Console.ReadLine();
+          
+                try
+                {
+                    result = eChannel.SendEvent(new KubeMQ.SDK.csharp.Events.Event()
+                    {
+                        //KubeMQ.SDK.csharp.ToolsConverter.ToByteArray, be sure do decode with the same encoder on sub.
+                        Body = Converter.ToByteArray(msg)
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[Demo] Could not send message, please check KubeMQ address, Error{ex.GetType()}");
+                    continue;
+                }
+
+                if (!result.Sent)
+                {
+                    Console.WriteLine($"[Demo] Could not send message:{result.Error}");
+
+                }
+
+                Console.WriteLine($"[Demo] Sent message ID:{result.EventID}");
+            }
+
+        }       
+    }
+}
--- KubeMQsub/Program.cs
+++ KubeMQsub/Program.cs
@@ -1,27 +1,28 @@
-﻿using System;
-using System.Text;
-using System.Threading;
-using KubeMQ.SDK.csharp.Events;
+﻿using KubeMQ.SDK.csharp.Events;
 using KubeMQ.SDK.csharp.Subscription;
 using KubeMQ.SDK.csharp.Tools;
+using System;
 
-namespace Demo
+namespace KubeMQsub
 {
     class Program
-    {
-        //The KubeMQ communication channel between pub sub
+    {  //The KubeMQ communication channel between pub sub
         const string eventChannelName = "TestCh_1";
+        private static bool subLoop=true;
+
         static void Main(string[] args)
         {
+
 
             //KubeMQ address can be set as environment parameter "KubeMQServerAddress"
             //The address port is the Grpc communication KubeMQ port 
             Console.WriteLine($"[Demo] KUBEMQ GRPC address:{Environment.GetEnvironmentVariable("KubeMQServerAddress")}");
-    
-            
+
+
             //Start event subscriber
 
             Console.WriteLine("[Demo] Starting demo SUB");
+
             //create KubeMQ.SDK.csharp.Subscription.SubscribeRequest
             SubscribeRequest subscribeRequest = new SubscribeRequest()
             {
@@ -45,54 +46,18 @@ namespace Demo
             catch (Exception ex)
             {
                 Console.WriteLine($"[Demo] Did not subscribe, please check KubeMQ address, Error:{ex.GetType()}");
-                goto End;
             }
 
-
-
-            ///Starting sync publisher
-            Console.WriteLine("[Demo] Starting demo PUB");          
-            ChannelParameters eventChannelParameters = new ChannelParameters
-            {
-                //Publish channel name (must match the sub ChannelName)
-                ChannelName = eventChannelName,
-                //pub name ID
-                ClientID = "pub_Demo"
-            };
-
-
-
-            //Create a new KubeMQ.SDK.csharp.Events.Channel 
-            var eChannel = new Channel(eventChannelParameters);
-            //KubeMQ.SDK.csharp.Events.Result is the pub message status.
-            Result result = null;
-            string msg = $"Hello there";
-            try
-            {   
-                result = eChannel.SendEvent(new KubeMQ.SDK.csharp.Events.Event()
-                {
-                    //KubeMQ.SDK.csharp.ToolsConverter.ToByteArray, be sure do decode with the same encoder on sub.
-                    Body = Converter.ToByteArray(msg)
-                });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"[Demo] Could not send message, please check KubeMQ address, Error{ex.GetType()}");
-                goto End;
-            }
-
-            if (!result.Sent)
-            {
-                Console.WriteLine($"[Demo] Could not send message:{result.Error}");
-                goto End;
-            }
-           
-            Console.WriteLine($"[Demo] Sent message ID:{result.EventID}");
-            
-
-            End:
             Console.WriteLine("[Demo] press Ctrl+c to stop");
-            Console.ReadKey();           
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+            {
+                subLoop = false;
+                e.Cancel = true;
+            };
+            while (subLoop)
+            {
+                Console.ReadKey();
+            }
 
         }
     }
