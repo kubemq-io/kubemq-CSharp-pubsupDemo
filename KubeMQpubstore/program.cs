2e3070ba4c4f84bb95b5257b0f8dcf4250e9d88a
Ido-Sheffer <ido.sheffer@kubemq.io>
2019-06-17 14:48:30 +0300

    added demo for persistant event store patteren

---
+++ KubeMQpubstore/program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using KubeMQ.SDK.csharp.Events;
+using KubeMQ.SDK.csharp.Tools;
+
+namespace KubeMQpubstore
+{
+    class Program
+    {
+
+
+        //The pub ClientID, can be set by environment var CLIENT
+        private static string ClientID = Environment.GetEnvironmentVariable("CLIENT") ?? $"pub_Demo_{Environment.MachineName}";
+        //The KubeMQ communication channel between pub sub, can be set by environment var CHANNEL 
+        private static string ChannelName = Environment.GetEnvironmentVariable("CHANNEL") ?? "Test_Channel";
+
+        private static bool pubLoop = true;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Demo] Start demo PUB");
+            //KubeMQ address environment var "KubeMQServerAddress" GRPC port
+            Console.WriteLine($"[Demo] KUBEMQ GRPC address:{Environment.GetEnvironmentVariable("KubeMQServerAddress")}");
+            Console.WriteLine($"[Demo] ClientID:{ClientID}");
+            Console.WriteLine($"[Demo] ChannelName:{ChannelName}");
+
+            Console.WriteLine("[Demo] press Ctrl+c to stop");
+
+            System.Threading.AutoResetEvent waitHandle = new System.Threading.AutoResetEvent(false);
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+            {
+                pubLoop = false;
+                e.Cancel = true;
+                waitHandle.Set();
+
+            };
+
+            ChannelParameters channelParameters = new ChannelParameters()
+            {
+                ChannelName = ChannelName,
+                ClientID = ClientID,
+                Store = true
+            };
+
+
+            //Create a new KubeMQ.SDK.csharp.Events.Channel 
+            var eChannel = new Channel(channelParameters);
+            //KubeMQ.SDK.csharp.Events.Result is the pub message status.
+            Result result = null;
+
+            Console.WriteLine("[Demo] Please enter message text to publish");
+
+            while (pubLoop)
+            {
+                string msg = Console.ReadLine();
+
+                if (msg == null)
+                {
+                    continue;
+                }
+                try
+                {
+                    result = eChannel.SendEvent(new KubeMQ.SDK.csharp.Events.Event()
+                    {
+                        //KubeMQ.SDK.csharp.ToolsConverter.ToByteArray, be sure do decode with the same encoder on sub.
+                        Body = Converter.ToByteArray(msg)
+
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[Demo] Could not send messages, please check KubeMQ address, Error{ex.GetType()}");
+                    waitHandle.Set();
+                    break;
+                }
+
+                if (!result.Sent)
+                {
+                    Console.WriteLine($"[Demo] Could not send single message:{result.Error}");
+                    continue;
+                }
+
+                Console.WriteLine($"[Demo] Sent message ID:{result.EventID}");
+            }
+
+            waitHandle.WaitOne();
+
+
+        }
+    }
+}
+
+
+    
+    
+
---
+++ KubeMQsubstore/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using KubeMQ.SDK.csharp.Events;
+using KubeMQ.SDK.csharp.Subscription;
+using KubeMQ.SDK.csharp.Tools;
+
+namespace KubeMQsubstore
+{
+    class Program
+    {
+
+        //The sub ClientID, can be set by environment var CLIENT
+        private static string ClientID = Environment.GetEnvironmentVariable("CLIENT") ?? $"sub_Demo_{Environment.MachineName}";
+        //The KubeMQ communication channel between pub sub, can be set by environment var CHANNEL 
+        private static string ChannelName = Environment.GetEnvironmentVariable("CHANNEL") ?? "Test_Channel";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Demo] Starting demo SUB");
+            //KubeMQ address environment var "KubeMQServerAddress" GRPC port         
+            Console.WriteLine($"[Demo] KUBEMQ GRPC address:{Environment.GetEnvironmentVariable("KubeMQServerAddress")}");
+            Console.WriteLine($"[Demo] ClientID:{ClientID}");
+            Console.WriteLine($"[Demo] ChannelName:{ChannelName}");
+
+            Console.WriteLine("[Demo] press Ctrl+c to stop");
+            System.Threading.AutoResetEvent waitHandle = new System.Threading.AutoResetEvent(false);
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+            {
+                e.Cancel = true;
+                waitHandle.Set();
+            };
+
+
+
+            //create KubeMQ.SDK.csharp.Subscription.SubscribeRequest
+            SubscribeRequest subscribeRequest = new SubscribeRequest()
+            {
+                Channel = ChannelName, //Subscribed channel name
+                SubscribeType = SubscribeType.EventsStore, //sub channel pattern
+                ClientID = ClientID,  //sub name ID
+                EventsStoreType = EventsStoreType.StartFromFirst,
+                EventsStoreTypeValue = 0
+
+                
+            };
+
+            //create a new KubeMQ.SDK.csharp.Events.Subscriber (KubeMQServerAddress environment var)
+            Subscriber subscriber = new Subscriber();
+            try
+            {
+                /// SubscribeToEvents HandleEventDelegate anonymous 
+                subscriber.SubscribeToEvents(subscribeRequest, (eventReceive) =>
+                {
+                    //KubeMQ.SDK.csharp.ToolsConverter.Converter.FromByteArray decode message from byte[]
+                    var recMsg = Converter.FromByteArray(eventReceive.Body);
+                    Console.WriteLine($"[Demo] Receive message ID:{eventReceive.EventID} received:{recMsg}");
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Demo] Did not subscribe, please check KubeMQ address, Error:{ex.GetType()}");
+            }
+
+            waitHandle.WaitOne();
+        }
+    }
+}
+
